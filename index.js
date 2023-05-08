5c33deb6489c42ee94323490b3c43919b7b5a23f
Jose Gaspar <sainc2016@gmail.com>
2023-05-08 18:13:43 -0500

    ajustando css y recomendaciones

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ function toggleMenu() {
   menuToggle.classList.toggle("active");
   sidebar.classList.toggle("active");
 }
+
+let list = document.querySelectorAll(".list")
+function setActiveClass(){
+  list.forEach((item) => {
+    item.classList.remove("active2");
+    this.classList.add("active2");
+  });
+}
+list.forEach( (item)=> 
+item.addEventListener("mouseover", setActiveClass))
+
+function toggleMenu2() {
+  const menuToggle = document.querySelector(".toggle");
+  const sidebar = document.querySelector(".navigation");
+  menuToggle.classList.toggle("active2");
+  sidebar.classList.toggle("active2");
+}
